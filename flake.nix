107f32ef198980f0de64d653390d12e987057d5f
Emile Clark-Boman <eclarkboman@gmail.com>
2026-01-29 16:56:49 +1000

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,91 @@
+{
+  description = "Catch fish in Terraria legally and automatically!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    defaultSystems = ["aarch64-darwin" "aarch64-linux" "i686-linux" "x86_64-darwin" "x86_64-linux"];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs defaultSystems (system:
+        f system (import nixpkgs {
+          inherit system;
+          overlays = self.overlays;
+        }));
+  in {
+    overlays = [
+      (
+        final: prev: rec {
+          # https://github.com/NixOS/nixpkgs/blob/c339c066b893e5683830ba870b1ccd3bbea88ece/nixos/modules/programs/nix-ld.nix#L44
+          # > We currently take all libraries from systemd and nix as the default.
+          pythonldlibpath = with prev;
+            lib.makeLibraryPath [
+              # for python:
+              acl
+              attr
+              bzip2
+              curl
+              libsodium
+              libssh
+              libxml2
+              openssl
+              stdenv.cc.cc
+              systemd
+              util-linux
+              xz
+              zlib
+              zstd
+              # for PyQT6/PySide6:
+              dbus
+              fontconfig
+              freetype
+              glib
+              libGL
+              libxkbcommon
+              xorg.libX11
+            ];
+          # here we are overriding python program to add LD_LIBRARY_PATH to it's env
+          python-ld = prev.stdenv.mkDerivation {
+            name = "python";
+            buildInputs = [prev.makeWrapper];
+            src = prev.python311;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r $src/* $out/
+              wrapProgram $out/bin/python3 --set LD_LIBRARY_PATH ${pythonldlibpath}
+              wrapProgram $out/bin/python3.11 --set LD_LIBRARY_PATH ${pythonldlibpath}
+            '';
+          };
+          poetry-ld = prev.stdenv.mkDerivation {
+            name = "poetry";
+            buildInputs = [prev.makeWrapper];
+            src = prev.poetry;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r $src/* $out/
+              wrapProgram $out/bin/poetry --set LD_LIBRARY_PATH ${pythonldlibpath}
+            '';
+          };
+        }
+      )
+    ];
+
+    devShells = forAllSystems (
+      system: pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            python-ld # here python will be taken from the overlay up here
+            poetry-ld
+          ];
+
+          shell = "${pkgs.bash}/bin/bash";
+        };
+      }
+    );
+  };
+}
