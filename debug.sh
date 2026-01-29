46237f4a71ed87f99bb7b6c1e7bbbf9db8357a0e
Emile Clark-Boman <eclarkboman@gmail.com>
2026-01-29 22:15:50 +1000

    maybe the children are arguing?

--- debug.sh
+++ debug.sh
@@ -3,9 +3,11 @@ mkdir -p logs
 
 printf -v date '%(%Y:%m:%d-%H:%M:%S)T\n' -1
 
-valgrind -s                    \
+valgrind -s                              \
   --log-file="logs/coredump-${date}.log" \
-  --leak-check=full            \
-  --show-leak-kinds=all        \
-  --track-origins=yes          \
+  --leak-check=full                      \
+  --show-leak-kinds=all                  \
+  --track-origins=yes                    \
+  --trace-children=yes                   \
+  --child-silent-after-fork=no           \
   ./dist/tAutoFisher/tAutoFisher
